8426fc23eca8c6c76f543d978a2c9f2f639fe4fb
GitGudBruhh <gitgudbruhh@tuta.io>
2025-03-30 00:36:10 +0530

    Parameterized AES/RSA master key (Python)

--- design/ectf25_design/gen_subscription.py
+++ design/ectf25_design/gen_subscription.py
@@ -10,17 +10,22 @@ own risk!
 Copyright: Copyright (c) 2025 The MITRE Corporation
 """
 
+import os
 import argparse
 import json
 from pathlib import Path
 import struct
-from Crypto.PublicKey import RSA as EncAlgo
-from Crypto.Cipher import PKCS1_v1_5 as PadAlgo
+
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
+
 from Crypto.Cipher import AES
-import os
+from Crypto.Util.Padding import pad, unpad
 
 from loguru import logger
-rsa=0
+
+master_key_type = "AES"
+
 def load_keys(secrets, device_id_str, channel_str):
     """Load the master key, channel key, and initialization vector (IV) from the secrets.
 
@@ -35,14 +40,17 @@ def load_keys(secrets, device_id_str, channel_str):
     channel_details = secrets["channel_details"]
     decoder_details = secrets["decoder_details"]
 
-    # Load the private master key
-    if(rsa):
-        master_key_str = decoder_details[device_id_str]["master_key_encoder"]
-        if master_key_str is None:
-            raise ValueError("No Master key found for device")
-        master_key_encoder = EncAlgo.import_key(master_key_str)
+    # Load the private encoder master key
+    master_key_str = decoder_details[device_id_str]["master_key_encoder"]
+    if master_key_str is None:
+        raise ValueError("No Master key found for device")
+
+    if master_key_type == "RSA":
+        master_key_encoder = RSA.import_key(master_key_str)
+    elif master_key_type == "AES":
+        master_key_encoder = bytes.fromhex(master_key_str)
     else:
-        master_key_encoder = bytes.fromhex(decoder_details[device_id_str]["master_key"])
+        ValueError(f"Master Key type {master_key_type} undefined")
 
     # Load channel key
     channel_key_hex_str = channel_details[channel_str]["channel_key"]
@@ -84,45 +92,44 @@ def encrypt_subscription_struct(master_key_encoder, packed_data):
     :param packed_data: The packed binary data to be encrypted.
     :return: Encrypted subscription data.
     """
-    print(len(packed_data))
-    print(len(master_key_encoder))
-    # Create a cipher for encryption (EncAlgo)
-    if(rsa):
-        cipher = PadAlgo.new(master_key_encoder)
-        return cipher.encrypt(packed_data)
-    else:
-        cipher = AES.new(master_key_encoder, AES.MODE_ECB)
-        pad_length = 16 - (len(packed_data) % 16)
-        packed_padded = packed_data + bytes([pad_length] * pad_length)
-        encrypted_data = cipher.encrypt(packed_padded)
-        return encrypted_data
 
-def verify_encrypted_sub(decoder_details, device_id_str, encrypted_data, expected_values):
+    # Create a cipher for encryption
+    if master_key_type == "RSA":
+        cipher = PKCS1_v1_5.new(master_key_encoder)
+        return cipher.encrypt(packed_data)
+
+    if master_key_type == "AES":
+        cipher = AES.new(master_key_encoder, AES.MODE_ECB)
+        packed_padded = pad(packed_data, AES.block_size)
+        return cipher.encrypt(packed_padded)
+
+def verify_encrypted_sub(master_key_decoder, encrypted_data, expected_values):
     """Verify the decrypted subscription data against expected values.
 
-    :param decoder_details: Dictionary containing decoder details.
-    :param device_id_str: String representation of the device ID.
+    :param master_key_decoder: String representation of master key (decoder).
     :param encrypted_data: Encrypted subscription data to be verified.
     :param expected_values: Dictionary of expected values for verification.
     :raises AssertionError: If any of the unpacked values do not match the expected values.
     """
 
-    # Load the decoder master key
-    master_key_decoder = decoder_details[device_id_str]["master_key_decoder"]
-    master_key_decoder = EncAlgo.import_key(master_key_decoder)
+    # Decrypt based on algorithm used
+    if master_key_type == "RSA":
+        master_key_decoder = RSA.import_key(master_key_decoder)
+        cipher_decoder = PKCS1_v1_5.new(master_key_decoder)
+        decrypted_data = cipher_decoder.decrypt(encrypted_data)
 
-    # Create cipher for decryption
-    cipher_decoder = PadAlgo.new(master_key_decoder)
-    decrypted_data = cipher_decoder.decrypt(encrypted_data)
+    elif master_key_type == "AES":
+        print(master_key_decoder)
+        cipher_decoder = AES.new(bytes.fromhex(master_key_decoder), AES.MODE_ECB)
+        decrypted_data = cipher_decoder.decrypt(encrypted_data)
+        decrypted_data = unpad(decrypted_data, AES.block_size)
 
-    unpacked_data = struct.unpack(f"<IQQIQQQQ", decrypted_data)
+    unpacked_data = struct.unpack(f"<IQQI16s16s", decrypted_data)
 
     # Verify each value
     for key, expected_value in expected_values.items():
         actual_value = unpacked_data[["device_id", "start", "end",
-                                      "channel", "channel_key_upper",
-                                      "channel_key_lower", "iv_upper",
-                                      "iv_lower"].index(key)]
+                                      "channel", "channel_key", "iv"].index(key)]
         assert actual_value == expected_value, f"{key} does not match: expected {expected_value}, got {actual_value}"
 
         logger.debug(f"Verified {actual_value}")
@@ -158,20 +165,18 @@ def gen_subscription(secrets, device_id, start, end, channel):
     encrypted_data = encrypt_subscription_struct(master_key_encoder, packed_data)
 
     # Verify the encrypted subscription file
+    expected_values = {
+        "device_id": device_id,
+        "start": start,
+        "end": end,
+        "channel": channel,
+        "channel_key": bytes.fromhex(channel_key_hex_str),
+        "iv": bytes.fromhex(iv_hex_str),
+    }
 
-    # expected_values = {
-    #     "device_id": device_id,
-    #     "start": start,
-    #     "end": end,
-    #     "channel": channel,
-    #     "channel_key_upper": int(channel_key_hex_str[0:16], 16),
-    #     "channel_key_lower": int(channel_key_hex_str[16:], 16),
-    #     "iv_upper": int(iv_hex_str[0:16], 16),
-    #     "iv_lower": int(iv_hex_str[16:], 16)
-    # }
-    #
-    #
-    # verify_encrypted_sub(secrets["decoder_details"], device_id_str, encrypted_data, expected_values)
+    master_key_decoder = secrets["decoder_details"][device_id_str]["master_key_decoder"]
+
+    verify_encrypted_sub(master_key_decoder, encrypted_data, expected_values)
 
     # Pack the subscription. This will be sent to the decoder with ectf25.tv.subscribe
     return encrypted_data
